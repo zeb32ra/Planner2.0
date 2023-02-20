4c800d760773d87144bd8d3f5ea6e38c9b56ed65
zeb32ra <zeb32ra@gmail.com>
2023-02-20 18:31:30 +0300

    All done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -44,48 +44,82 @@ namespace Ezhednevnik
 
         private void Adding_Click(object sender, RoutedEventArgs e)
         {
-            Zametka new_zam = new Zametka(Name_zam.Text, Description.Text, Date_pic.Text);
-            zametki.Add(new_zam);
-            Work_with_file.My_Serialize(zametki, "Zametki.json");
-            Zam_list.Items.Add(new_zam.name);
-            today_zametki.Add(new_zam);
-            Name_zam.Text = "";
-            Description.Text = "";
+            if (Date_pic.Text != "")
+            {
+                Zametka new_zam = new Zametka(Name_zam.Text, Description.Text, Date_pic.Text);
+                zametki.Add(new_zam);
+                Work_with_file.My_Serialize(zametki, "Zametki.json");
+                Zam_list.Items.Add(new_zam.name);
+                today_zametki.Add(new_zam);
+                Name_zam.Text = "";
+                Description.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("If you want to create a note, choose a date");
+            }
         }
 
         private void Editing_Click(object sender, RoutedEventArgs e) 
         {
-            Zametka z_for_edit = today_zametki[current_zam];
-            foreach(Zametka i in zametki)
+            try
             {
-                if(i.name == z_for_edit.name & i.description == z_for_edit.description & i.date == z_for_edit.date)
+                Zametka z_for_edit = today_zametki[current_zam];
+                foreach (Zametka i in zametki)
                 {
-                    i.name = Name_zam.Text;
-                    i.description = Description.Text;
-                    today_zametki[current_zam].name = Name_zam.Text;
-                    today_zametki[current_zam].description = Description.Text;
+                    if (i.name == z_for_edit.name & i.description == z_for_edit.description & i.date == z_for_edit.date)
+                    {
+                        i.name = Name_zam.Text;
+                        i.description = Description.Text;
+                        today_zametki[current_zam].name = Name_zam.Text;
+                        today_zametki[current_zam].description = Description.Text;
+                        break;
+                    }
                 }
+            }
+            catch(Exception) 
+            {
+                MessageBox.Show("If you want to edit something, choose what to edit");
             }
             Work_with_file.My_Serialize(zametki, "Zametki.json");
             Name_zam.Text = "";
             Description.Text = "";
-            foreach (Zametka i in today_zametki)
-            {
-                Zam_list.Items.Remove(i.name);
-            }
+            Zam_list.Items.Clear();
             Show_zam();
         }
 
         private void Deleting_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                Zametka z_for_del = today_zametki[current_zam];
 
+                for (int i = 0; i < zametki.Count; i++)
+                {
+                    if (zametki[i].name == z_for_del.name & zametki[i].description == z_for_del.description & zametki[i].date == z_for_del.date)
+                    {
+                        zametki.Remove(zametki[i]);
+                        today_zametki.Remove(today_zametki[current_zam]);
+                        break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("If you want to delete something, choose what to delete");
+            }
+            Work_with_file.My_Serialize(zametki, "Zametki.json");
+            Name_zam.Text = "";
+            Description.Text = "";
+            Zam_list.Items.Clear();
+            Show_zam();
         }
 
 
         private void Zam_list_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             current_zam = Zam_list.SelectedIndex;
-            if(current_zam != -1)
+            if(current_zam > -1)
             {
                 Name_zam.Text = today_zametki[current_zam].name;
                 Description.Text = today_zametki[current_zam].description;
